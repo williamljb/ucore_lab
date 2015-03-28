a3ebc15dfa64e8710c80933994ae13eff0f92d41
williamljb <williamm2006@126.com>
2015-03-28 18:33:24 +0800

    comments

--- labcodes/lab2/kern/mm/default_pmm.c
+++ labcodes/lab2/kern/mm/default_pmm.c
@@ -9,7 +9,7 @@
    usually split, and the remainder added to the list as another free block.
    Please see Page 196~198, Section 8.2 of Yan Wei Ming's chinese book "Data Structure -- C programming language"
 */
-// LAB2 EXERCISE 1: YOUR CODE
+// LAB2 EXERCISE 1: 2012011375
 // you should rewrite functions: default_init,default_init_memmap,default_alloc_pages, default_free_pages.
 /*
  * Details of FFMA
@@ -98,10 +98,11 @@ default_alloc_pages(size_t n) {
     if (page != NULL) {
         list_del(&(page->page_link));
         if (page->property > n) {
+            //TODO: set property and reserve bits
             struct Page *p = page + n;
             p->property = page->property - n;
             list_add(&free_list, &(p->page_link));
-    }
+        }
         nr_free -= n;
         ClearPageProperty(page);
     }
@@ -121,6 +122,7 @@ default_free_pages(struct Page *base, size_t n) {
     SetPageProperty(base);
     list_entry_t *le = list_next(&free_list);
     while (le != &free_list) {
+        //TODO: set reserve bits and could be faster
         p = le2page(le, page_link);
         le = list_next(le);
         if (base + base->property == p) {
